4f821ae0614f92a6040e3ee17510b7b8e3dd30c7
Dev-With-Shiv <128348282+Dev-With-Shiv@users.noreply.github.com>
2024-08-08 12:16:31 +0530

    Added comment in branch 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world1!");
         System.out.println("Hello world from second commit");
+        System.out.println("Test 3");
     }
 }
